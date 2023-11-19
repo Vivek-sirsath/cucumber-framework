1cba354487b18dffa1170fa1a105d0e5e2db385c
vivek <vivek.sirsath1@gmail.com>
2023-11-19 23:05:27 +0530

    Removed unused import statements

--- CucumberFramework/src/test/java/pageObject/AddNewCustomerPage.java
+++ CucumberFramework/src/test/java/pageObject/AddNewCustomerPage.java
@@ -1,12 +1,9 @@
 package pageObject;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
